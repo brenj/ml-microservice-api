7ced867499da3e7abee60a639c50bbe60860fd51
brenj <bwhite@nowsecure.com>
2020-03-08 20:33:02 -0400

    Update run_docker script
    * Adds commands to build and start container

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-## Complete the following steps to get Docker running locally
-
-# Step 1:
-# Build image and add a descriptive tag
-
-# Step 2: 
-# List docker images
-
-# Step 3: 
-# Run flask app
+docker build --tag=ml-microservice-api .
+docker image ls
+docker run -p 8000:80 ml-microservice-api
