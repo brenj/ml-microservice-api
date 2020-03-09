a84fa7c7b4ade5d45f2b5d8d402be8d6cc9b030e
brenj <bwhite@nowsecure.com>
2020-03-08 23:24:58 -0400

    Update upload_docker
    * Push docker container to hub

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --tag=ml-microservice-api .
+docker build --tag=brenj/ml-microservice-api .
 docker image ls
 docker run -p 8000:80 ml-microservice-api
--- scripts/upload_docker.sh
+++ scripts/upload_docker.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
-# This file tags and uploads an image to Docker Hub
+#
+# Tags and uploads an image built with "run_docker.sh" to Docker Hub.
 
-# Assumes that an image is built via `run_docker.sh`
+DOCKERPATH="brenj/ml-microservice-api"
 
-# Step 1:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
-
-# Step 2:  
-# Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
-
-# Step 3:
-# Push image to a docker repository
+echo "Docker ID and Image: ${DOCKERPATH}"
+docker login
+docker push "${DOCKERPATH}:latest"
